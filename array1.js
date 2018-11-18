81ac1227ce2655d6e0337d9b0e683b84194b7f9a
dayo77 <davidyonathan77@gmail.com>
2018-11-18 16:45:30 +0700

    file ini aku kunci ya

---
+++ array1.js
@@ -0,0 +1,27 @@
+let buah = [];
+
+buah[0] = 'nanas';
+buah[1] = 'strawberry';
+buah[2] = 'tomat';
+buah[3] = 'wortel';
+
+//buah.shift();
+//buah.pop();
+//buah.push();
+
+let y = buah.indexOf("wortel");
+//console.log(y);
+
+//console.log(buah.length);
+
+for (i = 0; i < buah.length; i++){
+    //console.log(" Nama buah index ke "+i+" adalah "+buah[i]);
+}
+
+//array multi dimensi
+let student = [];
+student[0] = [1,"Indra"];
+student[1] = [2,"Indra","bsd"];
+student[2] = [3,"Indra"];
+
+console.log(student);
